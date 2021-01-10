8a91c6425e399c4d4d8d7bd99b32110e1b10fb90
GUILHERME MARTINS <74570234+Guilherme339@users.noreply.github.com>
2021-01-10 18:24:08 -0300

    Create CartaoCreditoViewModel.cs

---
+++ Vaquinha.Domain/ViewModels/CartaoCreditoViewModel.cs
@@ -0,0 +1,10 @@
+namespace Vaquinha.Domain.ViewModels
+{
+    public class CartaoCreditoViewModel
+    {
+        public string NomeTitular { get; set; }
+        public string NumeroCartaoCredito { get; set; }
+        public string Validade { get; set; }
+        public string CVV { get; set; }
+    }
+}
