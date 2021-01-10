92afd549f75258430196bcef6c4b13d1afed6fe1
GUILHERME MARTINS <74570234+Guilherme339@users.noreply.github.com>
2021-01-10 18:23:38 -0300

    Create CausaViewModel.cs

---
+++ Vaquinha.Domain/ViewModels/CausaViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vaquinha.Domain.ViewModels
+{
+    public class CausaViewModel
+    {
+        public string Nome { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
